811e8fa82a74607accd8ab73b3a6e660d84f0f7a
clinconao <cl.linconao@gmail.com>
2023-10-15 00:32:27 +1300

    centrado y h1

--- preEntrega1Linconao/src/componentes/ItemListContainer.jsx
+++ preEntrega1Linconao/src/componentes/ItemListContainer.jsx
@@ -1,10 +1,11 @@
 
-
 export const ItemListContainer = ({greeting = "Bienvenido a nuestra tienda"}) => {
     return (
-        <>
+        <center>
+            <h1>
             {greeting}
-        </>
+            </h1>
+        </center>
     )
 }
 
